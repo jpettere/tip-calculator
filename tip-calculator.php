6827ba969d7f601de8e94c6369919ff51752fcc8
Julia Pettere <juliapettere@Julias-MacBook-Pro-3.local>
2016-12-27 19:28:03 -0800

    Added navigation bar

--- tip-calculator.php
+++ tip-calculator.php
@@ -16,7 +16,6 @@ if (isset($_POST["billamount"]) && isset($_POST["tippercentage"])) {
     $tip_percentage = $_POST["tippercentage"];
     $split = $_POST["splitamongst"];
 
-
     if (($bill_amount == "") || ($tip_percentage == "") ||
         !preg_match($bill_check, $_POST["billamount"])
     ) {
@@ -38,7 +37,6 @@ if (isset($_POST["billamount"]) && isset($_POST["tippercentage"])) {
         $split_tip = format($split_tip);
         $split_total = format($split_total);
     }
-
 }
 
 function format($value) {
@@ -56,75 +54,78 @@ function format($value) {
 <!DOCTYPE html>
     <html lang="en">
     <head>
-        <title>Tip Calculator</title>
+        <title>Easy Tip</title>
         <link type="text/css" href="tip-calculator.css" rel="stylesheet"/>
     </head>
 
     <body>
-    <div id="header">
-
-    </div>
-
-    <div id="main-content">
-        <h1>Tip Calculator</h1>
-        <form method="post" action="tip-calculator.php">
-            <div id="billamount">
-                <label>
-                    <strong>Bill Subtotal: $</strong>
-                    <input class="text" type="text" name="billamount" value="<?php echo $bill_amount ?>" size="15" autofocus>
-                </label>
-                <div class="error">
-                    <?php if ($bill_error != "") { ?>
-                        <p><?=$bill_error?></p>
-                        <?php
-                    } ?>
-                </div>
-            </div>
-            <div id="percentage">
-                <strong>Tip Percentage:</strong><br />
-                <?php
-                for ($i = 10; $i <= 20; $i+=5) { ?>
-                    <label>
-                        <input type="radio" name="tippercentage" value="<?= $i ?>"
-                            <?php if (isset($_POST["tippercentage"]) && $_POST["tippercentage"] == $i ||
-                                !isset($_POST["tippercentage"])
-                            ) { ?> checked <?php } ?> /><?= $i ?>%
-                    </label>
-                <?php }
-                ?>
-            </div>
-            <div id="split">
-                <label>
-                    Split amongst: <input class="text" type="text" value="<?=$split?>" name="splitamongst" size="15"/> person(s).
-                </label>
-                <div class="error">
-                    <?php if ($split_error != "") { ?>
-                        <p><?=$split_error?></p>
-                        <?php
-                    } ?>
-                </div>
-            </div>
-            <div id="submit">
-                <input type=submit value="Calculate Tip"/>
-            </div>
-        </form>
-        <?php if ($total != -1) { ?>
-        <div id="result">
-            <p><span>Tip: $</span><?= $tip ?></p>
-            <p><span>Total: $</span><?= $total ?></p>
-            <?php if ($split > 1) { ?>
-            <div id="splitresult">
-                <p><span>Tip each: $</span><?= $split_tip ?></p>
-                <p><span>Total each: $</span><?= $split_total ?></p>
-            </div>
-            <?php } ?>
+        <div id="header">
+            <ul class="navigation">
+                <li><a href="tip-calculator.php">Tip Calculator</a></li>
+                <li><a href="http://www.cntraveler.com/stories/2008-11-11/etiquette-101-tipping-guide"
+                       target="_blank">International Guide</a></li>
+                <li><a href="https://www.tripadvisor.com/Travel-g191-s606/United-States:Tipping.And.Etiquette.html"
+                    target="_blank">United States Guide</a></li>
+                <li><a href="about.html">About</a></li>
+            </ul>
         </div>
-            <?php
-        }
-        ?>
-    </div>
-
-    <div id="footer">
-    </div>
+        <div id="main-content">
+            <h1>Easy Tip</h1>
+            <form method="post" action="tip-calculator.php">
+                <div id="billamount">
+                    <label>
+                        <strong>Bill Subtotal: $</strong>
+                        <input class="text" type="text" name="billamount" value="<?php echo $bill_amount ?>" size="15" autofocus>
+                    </label>
+                    <div class="error">
+                        <?php if ($bill_error != "") { ?>
+                            <p><?=$bill_error?></p>
+                            <?php
+                        } ?>
+                    </div>
+                </div>
+                <div id="percentage">
+                    <strong>Tip Percentage:</strong><br />
+                    <?php
+                    for ($i = 10; $i <= 20; $i+=5) { ?>
+                        <label>
+                            <input type="radio" name="tippercentage" value="<?= $i ?>"
+                                <?php if (isset($_POST["tippercentage"]) && $_POST["tippercentage"] == $i ||
+                                    !isset($_POST["tippercentage"])
+                                ) { ?> checked <?php } ?> /><?= $i ?>%
+                        </label>
+                    <?php }
+                    ?>
+                </div>
+                <div id="split">
+                    <label>
+                        Split amongst: <input class="text" type="text" value="<?=$split?>" name="splitamongst" size="15"/> person(s).
+                    </label>
+                    <div class="error">
+                        <?php if ($split_error != "") { ?>
+                            <p><?=$split_error?></p>
+                            <?php
+                        } ?>
+                    </div>
+                </div>
+                <div id="submit">
+                    <input type=submit value="Calculate Tip"/>
+                </div>
+            </form>
+            <?php if ($total != -1) { ?>
+            <div id="result">
+                <p><span>Tip: $</span><?= $tip ?></p>
+                <p><span>Total: $</span><?= $total ?></p>
+                <?php if ($split > 1) { ?>
+                <div id="splitresult">
+                    <p><span>Tip each: $</span><?= $split_tip ?></p>
+                    <p><span>Total each: $</span><?= $split_total ?></p>
+                </div>
+                <?php } ?>
+            </div>
+                <?php
+            }
+            ?>
+        </div>
     </body>
 </html>
